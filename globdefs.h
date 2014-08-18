826c730a8c6e7e3c90e9f1a89a1e26c1cf4180dd
Gavin Smith <gavinsmith0123@gmail.com>
2014-08-18 18:37:12 +0100

    Initial commit

---
+++ globdefs.h
@@ -0,0 +1,39 @@
+/* Allegro port Copyright (C) 2014 Gavin Smith. */
+
+/*-------------------------------------------------------*/
+/*                                                       */
+/*                G L O B D E F S . H                    */
+/*                                                       */
+/*            [c] Copyright by Alpha - Helix             */
+/*            written by Dany Schochini 1989             */
+/*                                                       */
+/*-------------------------------------------------------*/
+
+
+
+
+#define    FALSE         0
+#define    TRUE          (!FALSE)
+
+
+#ifndef BYTEDEF
+
+   typedef unsigned char     byte;
+   #define BYTEDEF
+
+#endif
+
+#ifndef ULONGDEF
+
+   typedef unsigned long     ulong;
+   #define ULONGDEF
+
+#endif
+
+
+#ifndef LOCAL
+
+   #define LOCAL(xx)         static xx near pascal
+
+#endif
+
